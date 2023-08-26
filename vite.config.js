4a82165b44d5d4ea1cee33735436d292366c4967
Joshua Stanford <stanjosh@gmail.com>
2023-08-26 18:36:49 -0500

    changed directories agin

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/main",
-  root: "./src/client/",
+  base: "/",
+  root: "./",
   build: {
-    outDir: "public/main/",
+    outDir: "public/",
     assetsDir: "assets/",
   },
 });
