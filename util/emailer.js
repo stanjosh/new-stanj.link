d16a33cb66eddc41fa85fd371d7b9534866e84fb
Joshua Stanford <stanjosh@gmail.com>
2023-09-17 21:39:01 -0500

    emailer limits

--- util/emailer.js
+++ util/emailer.js
@@ -11,13 +11,7 @@ const transporter = nodemailer.createTransport({
 	secure: true,
 });
 
-const validateEmail = (email) => {
-	return String(email)
-  .toLowerCase()
-  .match(
-		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-  );
-};
+0
 
 const sendEmail = async (email, name, subject, message) => {
   
